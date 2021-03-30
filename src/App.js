09c9ce464deb78cd346420537f9fd461a40d4608
Nonchalantcode <hectorg269s@outlook.com>
2021-03-30 16:57:16 -0600

    Refactored code to pull posts and comments from the PostList component as per suggestion

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import PostList from './components/PostList'
-import { getPosts } from './reducers/postsReducer'
-import { getComments } from './reducers/commentsReducer'
 import './main.scss'
 
 function App() {
-  const dispatch = useDispatch()
-  const posts = useSelector(state => state.posts)
-
-  const isEmpty = coll => coll.length === 0
-
-  useEffect(() => {
-    dispatch(getPosts())
-    dispatch(getComments())
-  }, [dispatch])
-
   return (
     <div className="App">
       <h1 className="text-center">Our Posts</h1>
-      {
-        isEmpty(posts)
-          ? <h2 className="text-center">There are no posts here. Try refreshing the page or check your internet connection</h2>
-          : <PostList posts={posts} />
-      }
+      <PostList />
     </div>
   );
 }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import PostModal from './PostModal'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPosts } from '../reducers/postsReducer'
+import { getComments as retrieveAllComments } from '../reducers/commentsReducer'
 
-const PostList = ({posts}) => {
+const PostList = () => {
+    const posts = useSelector(state => state.posts)
+    const dispatch  = useDispatch()
+
+    const isEmpty = coll => coll.length === 0
+
+    useEffect(() => {
+        dispatch(getPosts())
+        dispatch(retrieveAllComments())
+    }, [dispatch])
+
     return (
-        <ul className="posts-container">
-            {
-                posts.map(post => 
-                    <li key={uuidv4()}>
-                        <div className="post">
-                            <h3 className="post-title">{ post.title }</h3>
-                            <div className="post-summary">{ post.body.slice(0, 50)}...</div>
-                            <div className="open">
-                                <PostModal post={post} />
+        isEmpty(posts)
+            ? <h2 className="text-center">There are no posts here. Try refreshing the page or check your internet connection</h2>
+            : <ul className="posts-container">
+                {
+                    posts.map(post => 
+                        <li key={uuidv4()}>
+                            <div className="post">
+                                <h3 className="post-title">{ post.title }</h3>
+                                <div className="post-summary">{ post.body.slice(0, 50)}...</div>
+                                <div className="open">
+                                    <PostModal post={post} />
+                                </div>
                             </div>
-                        </div>
-                    </li>    
-                )
-            }
-        </ul>
+                        </li>    
+                    )
+                }
+            </ul>
     )
 }
 
